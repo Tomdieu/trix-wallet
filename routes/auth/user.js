e0956de24f959a35d94dfa87b65ece076e6cc49f
Tomdieu <ivantomdio@gmail.com>
2022-12-17 17:03:48 +0100

    updated

--- routes/auth/user.js
+++ routes/auth/user.js
@@ -1,16 +1,25 @@
-const express = require('express')
-const User = require('../../models/User');
+const express = require("express");
+const User = require("../../models/User");
 
-const router = express.Router()
+const userSchema = require('../../models/schema/userSchema')
+const {validate} = require('../../models/validation')
 
-router.post('/register',(req,res)=>{
-    console.log(req.body);
-    res.status(200).json({success:true,data:[]})
-})
 
-router.get('/:id',(req,res)=>{
-    console.log(req.params);
-    res.status(200).send({success:true,data:[]})
-})
+const router = express.Router();
+
+
+
+router.post("/register", validate(userSchema), (req, res) => {
+
+  res.status(200).json({
+    success: true,
+    message: "Registration successful",
+  });
+});
+
+router.get("/:id", (req, res) => {
+  console.log(req.params);
+  res.status(200).send({ success: true, data: [] });
+});
 
 module.exports = router;
