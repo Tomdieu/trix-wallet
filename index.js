85c043f46b1d6c4f2b79f305f42c44d1a152519e
Tomdieu <ivantomdio@gmail.com>
2022-12-22 23:24:41 +0100

    updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cron = require("node-cron");
-const cors = require('cors')
-// const swaggerDoc = require('./utils/swagger')
+const cors = require("cors");
+const swaggerDoc = require("./utils/docs.js");
 
 const routes = require("./routes");
 const { cancelAllWithdrawals } = require("./tasks");
@@ -9,18 +9,16 @@ const { cancelAllWithdrawals } = require("./tasks");
 require("./models");
 
 const app = express();
-const server = require("http").Server(app)
-
+const server = require("http").Server(app);
 
 // Cors
-app.use(cors())
+app.use(cors());
 
 // parse json
 app.use(express.json());
 
 //parse form data
 app.use(express.urlencoded({ extended: false }));
-
 
 const PORT = process.env.PORT | 5000;
 
@@ -30,23 +28,22 @@ app.get("/", (req, res) => {
   res.status(200).send("<h1>Welcome To My Nodejs TrixWallet API</h1>");
 });
 
-
 // run automatic tasks
-cron.schedule("*/15 * * * * *",() => {
+cron.schedule("*/15 * * * * *", () => {
   // cancel all the withdrawals
 
-  cancelAllWithdrawals().then(()=>{})
+  cancelAllWithdrawals().then(() => {});
 });
 
+const instance = swaggerDoc(app);
 
+// Sockets
+const Socket = require("./sockets");
+Socket(server);
 
-
+// module.exports = { instance };
+exports.instance = instance;
 
 server.listen(PORT, async () => {
   console.log(`Server running on PORT ${PORT} at http://127.0.0.1:${PORT}`);
-  
-  // Sockets 
-  const Socket = require("./sockets")
-  Socket(server)
-  // swaggerDoc(app,PORT)
 });
