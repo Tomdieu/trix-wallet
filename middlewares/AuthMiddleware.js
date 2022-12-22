63e24e8c907b58cf681b5b65a15babf04bad1297
Tomdieu <ivantomdio@gmail.com>
2022-12-22 23:23:51 +0100

    updated so that it consider token and Bearer in the headers

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -1,30 +1,28 @@
 const AuthMiddleWare = async (req, res, next) => {
   if (!req.headers["authorization"]) {
-    res
-      .status(400)
-      .send({ error: true, message: "Authorization require" });
+    res.status(400).send({ success: false, message: "Authorization require" });
   } else {
     const [_, token] = req.headers["authorization"].split(" ");
-    if (_ !== "token" || !token) {
+    if ((_ !== "token" && !token) || _ !== "Bearer" || !token) {
       res
         .status(400)
-        .send({ error: true,success:false, message: "Authorization required" });
+        .send({ success: false, message: "Authorization required" });
     } else {
       const { Token, User } = require("../models/");
       const tk = await Token.findOne({ where: { key: token } });
       if (tk) {
         const user = await User.findByPk(tk.user_id);
-        req.user = user
+        req.user = user;
         // const authenticated_user = user.toJSON();
         // req.user = authenticated_user;
         next();
       } else {
         res
           .status(400)
-          .send({ error: true, success:false,message: "Please provide a valid token" });
+          .send({ success: false, message: "Please provide a valid token" });
       }
     }
   }
 };
 
-module.exports = AuthMiddleWare
+module.exports = AuthMiddleWare;
