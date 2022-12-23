0b065fa02cecf99cef5f72e220d5a07eb7a032dc
Tomdieu <ivantomdio@gmail.com>
2022-12-23 07:57:40 +0100

    updated

---
+++ test/test_1.js
@@ -0,0 +1,32 @@
+const {expect} = require('chai')
+const { it } = require('mocha')
+process.env.NODE_ENV = 'TESTING'
+// console.log(process.env)
+
+describe('Test 1',async ()=>{
+    const {User,Token}=require('../models')
+
+    describe('Addition', async () => {
+        it('Should add 1+1',()=>{
+            const result = 1+1;
+
+            expect(result).to.be.equal(2)
+        })
+    })
+    
+    describe('Multiplication', () => {
+        it('Should add 1*1',()=>{
+            const result = 1*1;
+            expect(result).to.be.equal(1)
+        })
+    })
+
+    describe('List of users',async ()=>{
+        it('Should Return a list of users',async ()=>{
+            var users = await User.findAll()
+
+            users =  users.map(d=>d.toJSON())
+            expect(users.length).to.be.above(2)
+        })
+    })
+})
